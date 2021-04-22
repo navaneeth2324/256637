ce1ebf9ae67d6073e81b700020b41eb0c20e41b9
navaneeth2324 <navneeth023@gmail.com>
2021-04-22 12:18:05 +0530

    list datastructure

---
+++ lists.py
@@ -0,0 +1,11 @@
+mylist = [] #empty list
+mylist.append(1)  #adding element to a list using append() method
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints each element of a list using index
+print(mylist[1]) 
+print(mylist[2]) 
+
+# printing using the for loop by its indices
+for x in mylist:
+    print(x)
