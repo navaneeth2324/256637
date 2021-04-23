f03234200125f9b1335bf0a7cb0f79967e175d45
navaneeth2324 <navneeth023@gmail.com>
2021-04-23 12:28:18 +0530

    Added comments

--- submission1.py
+++ submission1.py
@@ -1,3 +1,5 @@
+#Write a Python script to merge two Python dictionaries
+# update function is used to merge 2 dictionaries
 dict1={"Mon":1,"Tue":2,"Wed":3,"Thu":4}
 dict2={"Fri":5,"Sat":6,"Sun":7}
 print(dict1)
