23dc4a0b62c2099f70b0aaf3476320cde8fd844b
navaneeth2324 <navneeth023@gmail.com>
2021-04-26 12:52:33 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,15 @@
+##Arithmetic Operators
+num = 1 + 2 * 3 / 4.0
+print(num)
+remainder = 11 % 3
+print(remainder)
+
+##Power operation using **
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+#String multiplication using *
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
